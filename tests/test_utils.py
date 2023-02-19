594e33cc81f89271225f6b1f0ecb8cc390f5f907
Justin Speegle <justin@speegs.com>
2023-02-18 17:29:26 -0800

    unit test cleanse_filename

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,38 +1,22 @@
 import pytest
+import yaml
 from utils import cleanse_filename
 
 
 TESTDATA = 'tests/testdata.yaml'
 
 
-def parse_yaml(file='metadata.yaml') -> dict:
+def data(cls: str, test: str, file=TESTDATA):
     with open(file, 'r') as fl:
         dict = yaml.load(fl, Loader=yaml.FullLoader)
-    return dict
+    list = dict.get(cls).get(test)
+    return {'ids': [i.get('id') for i in list],
+            'test': [(k, v) for item in list
+                     for k, v in item.get('test').items()]}
 
 
-TESTDATA = {'a:k_a-a)a(a a': 'a:k_a_a_a_a_a'}
-td = [(_in, _out) for _in, _out in TESTDATA.items()]
-
-@pytest.mark.parametrize("_in,_out", td)
+cls, test = ('CleanseFilename', 'test_cleanse_filename')
+@pytest.mark.parametrize('_in,_out', data(cls, test).get('test'),
+                         ids = data(cls, test).get('ids'))
 def test_cleanse_filename(_in, _out):
     assert cleanse_filename(_in) == _out
-
-# class TestCleanseFilename():
-
-#     @pytest.fixture
-#     def testdata(self,  l):
-#         # td = parse_yaml(TESTDATA).get('CleanseFilename').get('test_cleanse_filename')
-#         td = TESTDATA
-#         return [(_in, _out) for _in, _out in td.items()]
-
-#     @pytest.mark.parametrize("_in,_out", testdata)
-#     def test_clease_filename(self, _in, _out):
-#         '''
-#         Test cases
-#         - special characters replaced with _
-#         - repeated special characters reduced to a single _
-#         - leading and trailing _ removed
-#         - returns lowcase
-#         '''
-#         assert cleanse_filename(_in) == _out
