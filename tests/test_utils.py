4bdab9adfe39a946d91b53e74bd881f3a854b8e4
Justin Speegle <justin@speegs.com>
2023-02-19 12:21:22 -0800

    unittest wip

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,22 +1,47 @@
 import pytest
 import yaml
-from utils import cleanse_filename
+from utils import cleanse_filename, write_file
 
 
 TESTDATA = 'tests/testdata.yaml'
 
 
-def data(cls: str, test: str, file=TESTDATA):
-    with open(file, 'r') as fl:
-        dict = yaml.load(fl, Loader=yaml.FullLoader)
-    list = dict.get(cls).get(test)
-    return {'ids': [i.get('id') for i in list],
-            'test': [(k, v) for item in list
-                     for k, v in item.get('test').items()]}
+class TestCleanseFilename():
+    def get_data(cls: str, test: str, file=TESTDATA):
+        with open(file, 'r') as fl:
+            dict = yaml.load(fl, Loader=yaml.FullLoader)
+        list = dict.get(cls).get(test)
+        return {'ids': [i.get('id') for i in list],
+                'test': [(k, v) for item in list
+                         for k, v in item.get('test').items()]}
+
+    cls, test = ('CleanseFilename', 'test_cleanse_filename')
+    data = get_data(cls, test)
+    @pytest.mark.parametrize('_in,_out', data.get('test'), ids=data.get('ids'))
+    def test_cleanse(self, _in, _out):
+        assert cleanse_filename(_in) == _out
 
 
-cls, test = ('CleanseFilename', 'test_cleanse_filename')
-@pytest.mark.parametrize('_in,_out', data(cls, test).get('test'),
-                         ids = data(cls, test).get('ids'))
-def test_cleanse_filename(_in, _out):
-    assert cleanse_filename(_in) == _out
+class TestWriteFile():
+    @pytest.fixture
+    def write(self, tmp_path, dry_run):
+        new_dir = tmp_path / 'new_dir'
+        write_file(new_dir, 'file.tmp', b'content', dry_run)
+        yield new_dir
+
+    @pytest.mark.parametrize('dry_run', [False])
+    def test_create_directory(self, write):
+        ''' - create directory'''
+        assert write.exists()
+
+    # def test_write_file():
+    #     ''' - write file'''
+    #     pass
+
+    # def test_dryrun_directory():
+    #     ''' - dry_run blocks directory creation'''
+    #     pass
+
+    # def test_dryrun_file():
+    #     ''' - dry_run blocks file creation'''
+    #     pass
